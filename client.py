4173f822957d3301a00598f455ebe5c70e9f93d5
mertyatir <mertyatir@hotmail.com>
2023-12-09 12:46:22 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,118 @@
+import argparse
+import pandas as pd
+import requests
+from datetime import datetime
+from io import StringIO
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill, Font
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', '--keys', nargs='*', default=[])
+parser.add_argument('-c', '--colored', type=bool, default=True)
+args = parser.parse_args()
+
+# Read CSV file
+df = pd.read_csv('vehicles.csv', delimiter=';')
+
+
+
+
+# Send POST request to server
+response = requests.post('http://localhost:5000/api', data=df.to_csv(index=False).encode('utf-8'))
+
+
+
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Convert response to DataFrame
+    df = pd.read_json(StringIO(response.text))
+else:
+    print(f"Error: Server returned status code {response.status_code}")
+    print(response.text)
+
+
+
+# Filter columns
+if "colorCode" in df.columns:
+    columns = ['rnr', 'hu','colorCode'] + args.keys
+else:
+    columns = ['rnr', 'hu'] + args.keys
+df = df[columns]
+
+# Sort DataFrame by 'gruppe' column
+if 'gruppe' in df.columns:
+    df = df.sort_values('gruppe')
+else:
+    print("Error: 'gruppe' column not found in DataFrame")
+
+
+
+# Check if 'hu' column exists
+if 'hu' in df.columns:
+    # Convert 'hu' column to datetime
+    df['hu'] = pd.to_datetime(df['hu'])
+    # Calculate the difference in months between 'hu' and the current date
+    df['hu_diff'] = (df['hu'].dt.year - pd.Timestamp.now().year) * 12 + df['hu'].dt.month - pd.Timestamp.now().month
+else:
+    print("Error: 'hu' column not found in DataFrame")
+
+# Check if 'hu_diff' column exists
+if 'hu_diff' in df.columns:
+    # Ensure 'hu_diff' is the first column
+    df = df[['hu_diff'] + [col for col in df.columns if col != 'hu_diff']]
+else:
+    print("Error: 'hu_diff' column not found in DataFrame")
+
+# Write to Excel file
+filename = f'vehicles_{datetime.now().isoformat()}.xlsx'
+
+
+
+dfWithDroppedColumns= df.drop(columns=['hu_diff'])
+
+
+if 'colorCode' in df.columns:
+    dfWithDroppedColumns= df.drop(columns=['colorCode','hu_diff'])
+else:
+    dfWithDroppedColumns= df.drop(columns=['hu_diff'])
+
+dfWithDroppedColumns.to_excel(filename, index=False)
+
+
+# Open the Excel file with openpyxl
+wb = load_workbook(filename)
+ws = wb.active
+
+# Apply color formatting
+if args.colored:
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
+        hu_diff = df.loc[row[0].row - 2, 'hu_diff']
+        if hu_diff <= 3:
+            fill = PatternFill(start_color="007500", end_color="007500", fill_type="solid")
+        elif hu_diff <= 12:
+            fill = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid")
+        else:
+            fill = PatternFill(start_color="b30000", end_color="b30000", fill_type="solid")
+        for cell in row:
+            cell.fill = fill
+
+# Check if 'labelIds' and 'colorCode' are in DataFrame columns and keys argument
+if 'labelIds' in df.columns and 'colorCode' in df.columns and 'labelIds' in args.keys:
+    # Iterate over each row in DataFrame
+    for index, row in df.iterrows():
+        # Check if 'colorCode' is not null
+        if pd.notnull(row['colorCode']):
+
+            print("row['colorCode']",row['colorCode'])
+            # Convert colorCode to ARGB hex value
+            color_code = row['colorCode'].lstrip('#')
+            if len(color_code) == 6:
+                color_code = 'FF' + color_code
+            # Use colorCode to tint the cell's text in Excel file
+            ws.cell(row=index+2, column=columns.index('labelIds')).font = Font(color=color_code)
+
+
+# Save the changes
+wb.save(filename)
---
+++ server.py
@@ -0,0 +1,209 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import requests
+from io import StringIO
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def api():
+    # Convert CSV data to DataFrame
+    csv_data = pd.read_csv(StringIO(request.data.decode('utf-8')))
+
+    print("CSV data",csv_data.shape)
+
+    # Authenticate
+    url = "https://api.baubuddy.de/index.php/login"
+    payload = {
+        "username": "365",
+        "password": "1"
+    }
+    headers = {
+        "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+        "Content-Type": "application/json"
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    access_token = response.json()["oauth"]["access_token"]
+
+    # Update headers with access token
+    headers["Authorization"] = f"Bearer {access_token}"
+
+    # Request resources
+    response = requests.get('https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active', headers=headers)
+
+    """
+    Response format:
+    [
+    {
+        "rnr": "10",
+        "gruppe": "Fahrzeuge",
+        "kurzname": "PB V 1300 (Container)",
+        "langtext": "MAN 26.440 Kennzeichen PB V 1300.",
+        "info": "Dieses Fahrzeug wurde für den Gerüstbau angeschafft.\nEs kann mit der alten Führerscheinklasse 2, bzw mit dem Führerschein Klasse C gefahren werden.\nDie Zuladung beträgt ca. 15.000 KG einschl. Container.\n\n\n!8.9.2023\nZu prüfen in regelmäßigen Abständen\n- Licht und Elektronik, \n- Auspuff, \n- Reifen, \n- Karosserie, \n- Innenraum, \n- Fahrwerk, Lenkung, Bremsen\n- Motorraum//.Öl und andere Flüssigkeiten",
+        "sort": "0",
+        "lagerort": "Paderborn",
+        "lteartikel": "Fahrzeug",
+        "businessUnit": "Gerüstbau",
+        "vondat": "2007-07-02",
+        "bisdat": null,
+        "hu": "2023-05-31",
+        "asu": "2021-12-01",
+        "createdOn": null,
+        "editedOn": "2023-10-25T09:37:48Z",
+        "fuelConsumption": "25.0",
+        "priceInformation": "0.0",
+        "safetyCheckDate": null,
+        "tachographTestDate": null,
+        "gb1": "Gerüstbau",
+        "ownerId": null,
+        "userId": null,
+        "externalId": "",
+        "vin": "",
+        "labelIds": null,
+        "bleGroupEnum": null,
+        "profilePictureUrl": "https://api.baubuddy.de/dev/infomaterial/Dokumente_vero_test/RNR_10/Bilder/IMG_20180120_0953251318581889.jpg",
+        "thumbPathUrl": "https://api.baubuddy.de/dev/infomaterial/Dokumente_vero_test/RNR_10/Bilder/Thumbs/IMG_20180120_0953251318581889.jpg"
+    },
+    ...
+    ]
+    
+    
+    """
+
+    mock_data = {
+    "rnr": "108",
+    "gruppe": "Werkzeuge",
+    "kurzname": "Makita DTW 190 Akkuschlagschrauber",
+    "langtext": "",
+    "info": "",
+    "sort": "0",
+    "lagerort": "Lager Werkzeuge",
+    "lteartikel": "Akku Schlagbohrschra",
+    "businessUnit": "Gerüstbau",
+    "vondat": "2023-10-10",
+    "bisdat": None,
+    "hu": "2025-10-31",
+    "asu": None,
+    "createdOn": "2023-10-10",
+    "editedOn": "2023-10-12T10:21:49Z",
+    "fuelConsumption": "0.0",
+    "priceInformation": "0.0",
+    "safetyCheckDate": None,
+    "tachographTestDate": None,
+    "gb1": "Gerüstbau",
+    "ownerId": None,
+    "userId": None,
+    "externalId": "",
+    "vin": "",
+    "labelIds": "134,133",
+    "bleGroupEnum": None,
+    "profilePictureUrl": "https://api.baubuddy.de/dev/infomaterial/Dokumente_vero_test/RNR_108/Bilder/2023-10-10_04-52-06-1000005472-209-108-1000005472.jpg",
+    "thumbPathUrl": "https://api.baubuddy.de/dev/infomaterial/Dokumente_vero_test/RNR_108/Bilder/Thumbs/2023-10-10_04-52-06-1000005472-209-108-1000005472.jpg"
+}
+    
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Convert the response to JSON
+
+        #data= [mock_data]
+        data = response.json()
+
+        # Convert the JSON data to a DataFrame
+        api_data = pd.DataFrame(data)
+    else:
+        print(f"/dev/index.php/v1/vehicles/select/active Error: Server returned status code {response.status_code}")
+
+
+
+    print("API data",api_data.shape)
+
+    # Merge CSV data and API data
+    merged_data = pd.concat([csv_data, api_data], ignore_index=True)
+
+    print("Merged data",merged_data.shape)
+
+    # Drop duplicate rows
+    merged_data.drop_duplicates(inplace=True)
+
+
+    print("Merged data after dropping duplicates",merged_data.shape)
+
+    # Before filtering out resources without 'hu'
+    print("Before filtering out resources without 'hu'",merged_data.shape)
+
+    # Filter out resources without 'hu'
+    if 'hu' in merged_data.columns:
+        merged_data = merged_data[merged_data['hu'].notna()]
+    else :
+        print("Error: 'hu' column not found")
+
+    
+
+    print("After filtering out resources without 'hu'",merged_data.shape)
+    
+
+
+    # Print nonNaN values for "labelIds"
+    print("NonNaN values for 'labelIds'",merged_data['labelIds'].dropna().unique())
+
+
+    # Resolve 'colorCode' for each 'labelId'
+    if 'labelIds' in merged_data.columns:
+        print("Resolving 'colorCode' for each 'labelId'")
+        for index, row in merged_data.iterrows():
+            print(f"Processing row {index}")
+            if row['labelIds'] is None:
+                continue
+            labelIds = row['labelIds'].split(',')  # Split the 'labelIds' string into a list
+            print("is List ", isinstance(row['labelIds'], list))
+            print("type ", type(row['labelIds']))
+            print("value ", row['labelIds'])
+            for labelId in labelIds:
+                response = requests.get(f'https://api.baubuddy.de/dev/index.php/v1/labels/{labelId}', headers=headers)
+
+                '''
+                Response format:
+                        [
+                            {
+                                    "id": 134,
+                                    "enum": "Werkzeuge",
+                                    "displayText": "Werkzeuge",
+                                    "colorCode": "",
+                                    "baseEntity": "resources",
+                                    "isAbstract": false,
+                                    "children": [
+                                        {
+                                            "id": 133,
+                                            "parentId": 134,
+                                            "enum": "Akkuschrauber",
+                                            "displayText": "Akkuschrauber",
+                                            "colorCode": "#ff9800",
+                                            "baseEntity": "resources"
+                                        }
+                                    ]
+                                }
+                            ]
+                                            
+                
+                '''
+                
+                if response.status_code == 200:
+                    response_data = response.json()
+                    if response_data:  # Check if the list is not empty
+                        first_dict = response_data[0]  # Access the first dictionary in the list
+                        colorCode = first_dict.get('colorCode')  # Get the 'colorCode'
+                        print(f"Resolved colorCode {colorCode} for label ID {labelId}")
+                        merged_data.loc[index, 'colorCode'] = colorCode
+                    else:
+                        print(f"Error: No data returned for label ID {labelId}")
+    else:
+        print("Error: 'labelIds' column not found")
+
+   
+
+    # Return processed data
+    return jsonify(merged_data.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
